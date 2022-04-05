ce88a45053d0345ec762c77edc849e74dcd1743c
subslog <100672617+subslog@users.noreply.github.com>
2022-04-06 02:19:51 +0900

    [Silver V] Title: 셀프 넘버, Time: 72 ms, Memory: 31888 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,21 +1,12 @@
 def seq(max: int):
-    '''n과 n의 각 자릿수를 더하는 함수'''
-    
-    # 셀프 넘버를 찾기 위한 리스트
-    self_no = list(range(1, max))
-    
-    for i in range(1, max):
-        seq_no = i  # 더한 결과값
+    '''셀프 넘버를 찾는 함수'''
 
-        # 각 자릿수를 더한다.
-        for j in str(i):
-            seq_no += int(j)
+    # 1 ~ max - 1 까지의 range
+    max_rang = range(1, max)
 
-        # 더한 결과값이 있으면 리스트에서 제거
-        if seq_no in self_no:
-            self_no.remove(seq_no)
-            
-    for no in self_no:
-        print(no)
+    # n과 n의 각 자릿수를 더한 결과 리스트
+    self_no = [i + sum(map(int, str(i))) for i in max_rang]
+    
+    print(*sorted(set(max_rang) - set(self_no)))
 
 seq(10000)
