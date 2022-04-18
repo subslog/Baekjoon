bd63237eeef47dc7333abb646ed6b450d8e1b88a
subslog <100672617+subslog@users.noreply.github.com>
2022-04-19 00:01:01 +0900

    [Bronze I] Title: 분수찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1193. 분수찾기/분수찾기.py
+++ 백준/Bronze/1193. 분수찾기/분수찾기.py
@@ -1,19 +1,18 @@
 X = int(input())
 
 an = 1     # 수열 an
-b = 2     # 증가 값
+b = 1     # 증가 값
 
 # 입력값이 수열 an보다 작거나 같을 때까지 반복
 while X > an:
-    an += b    # 계차수열만큼 증가
     b += 1    # 계차수열(+1)
+    an += b    # 계차수열만큼 증가
 
 sub = an - X   # 수열 an - 입력값
-n = b - 1     # n 번째
 
-# n이 홀수
-if n % 2 == 1:
-    print(f'{1 + sub}/{n - sub}')
+# b가 홀수
+if b % 2 == 1:
+    print(f'{1 + sub}/{b - sub}')
 # 짝수
 else:
-    print(f'{n - sub}/{1 + sub}')
+    print(f'{b - sub}/{1 + sub}')
