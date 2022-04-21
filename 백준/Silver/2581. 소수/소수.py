83db5c4862c3318a919a9140a125e10cc6502a4d
subslog <100672617+subslog@users.noreply.github.com>
2022-04-21 21:34:07 +0900

    [Silver V] Title: 소수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -4,15 +4,13 @@ N = int(input())
 if M == 1:
     M += 1
 
-prime_list = list(range(M, N + 1))
+prime_list = []
 
-for i in prime_list[:]:
+for i in range(M, N + 1):
     # 2부터 i의 제곱근 +1까지 반복
-    for j in range(2, int(i ** (1/2)) + 1):
-        # 약수가 있으면 리스트에서 i를 제거하고 내부 for 문 종료
-        if i % j == 0:
-            prime_list.remove(i)
-            break
+    # 약수가 없으면 소수 리스트에 추가
+    if all(i % j for j in range(2, int(i ** (1/2)) + 1)):
+        prime_list.append(i)
 
 if len(prime_list):
     print(f'{sum(prime_list)}\n{min(prime_list)}')
