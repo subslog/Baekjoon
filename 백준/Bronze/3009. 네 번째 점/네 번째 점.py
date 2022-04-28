1ec03d0851075231ddbcd913b0c81ef386aeb034
subslog <100672617+subslog@users.noreply.github.com>
2022-04-28 22:41:21 +0900

    [Bronze III] Title: 네 번째 점, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,41 +1,18 @@
-x1, y1 = map(int, input().split())
-x2, y2 = map(int, input().split())
-x3, y3 = map(int, input().split())
+import sys
 
-if x1 == x2:
-    other = x3, y3
-    if y1 > y2:
-        top = [x1, y1]
-        down = [x2, y2]
-    elif y1 < y2:
-        top = [x2, y2]
-        down = [x1, y1]
-elif x1 == x3:
-    other = x2, y2
-    if y1 > y3:
-        top = [x1, y1]
-        down = [x3, y3]
-    elif y1 < y3:
-        top = [x3, y3]
-        down = [x1, y1]
-elif x2 == x3:
-    other = x1, y1
-    if y2 > y3:
-        top = [x2, y2]
-        down = [x3, y3]
-    elif y2 < y3:
-        top = [x3, y3]
-        down = [x2, y2]
+x_list = []
+y_list = []
 
-if top[1] == other[1]:
-    if top[0] > other[0]:
-        result = [other[0], down[1]]
-    elif top[0] < other[0]:
-        result = [other[0], down[1]]
-elif down[1] == other[1]:
-    if top[0] > other[0]:
-        result = [other[0], top[1]]
-    elif top[0] < other[0]:
-        result = [other[0], top[1]]
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    if x in x_list:
+        x_list.remove(x)
+    else:
+        x_list.append(x)
+    
+    if y in y_list:
+        y_list.remove(y)
+    else:
+        y_list.append(y)
 
-print(*result)
+print(f'{x_list[0]} {y_list[0]}')
