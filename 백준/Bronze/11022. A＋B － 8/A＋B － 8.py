8933aa20d17c8551912ebf8b208cfb52d553f55d
subslog <100672617+subslog@users.noreply.github.com>
2022-06-13 23:39:27 +0900

    [Bronze III] Title: A+B - 8, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,7 +1,7 @@
 import sys
 
-T = int(input())
+T = int(sys.stdin.readline())
 
 for i in range(1, T + 1):
     A, B = map(int, sys.stdin.readline().split())
-    print(f'Case #{i}: {A} + {B} = {A + B}')
+    print(f"Case #{i}: {A} + {B} = {A + B}")
