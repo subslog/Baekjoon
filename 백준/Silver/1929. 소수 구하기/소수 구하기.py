fd422eae237fca4e590cacdccb6f537710d3d068
subslog <100672617+subslog@users.noreply.github.com>
2022-06-22 00:41:20 +0900

    [Silver III] Title: 소수 구하기, Time: 636 ms, Memory: 38512 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,9 +1,19 @@
 M, N = map(int, input().split())
 
-# M이 1이면 +1
+# M이 1이면 1 더하기
 if M == 1:
     M += 1
 
+prime_check = [False] * (N + 1)  # 소수 체크(False면 소수)
+
+# 2부터 N까지 소수 판단
+for i in range(2, N + 1):
+    if prime_check[i] == False:
+        # 소수의 배수 True로 변경
+        for j in range(i * 2, N + 1, i):
+            prime_check[j] = True
+
+# M부터 N까지 False인 요소가 소수
 for i in range(M, N + 1):
-    if all(i % j for j in range(2, int(i ** 0.5) + 1)):
+    if prime_check[i] == False:
         print(i)
