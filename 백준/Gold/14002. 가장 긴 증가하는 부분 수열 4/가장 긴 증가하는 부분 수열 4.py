95518c619724aaf687fb2dd01df42a2483138589
subslog <100672617+subslog@users.noreply.github.com>
2022-06-29 00:35:44 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 168 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,20 +1,27 @@
+def go(num: int) -> int:
+    """역추적 출력 함수"""
+
+    if num == -1: return
+
+    go(v[num])
+    print(A[num], end=" ")
+
 N = int(input())
 
 A = list(map(int, input().split()))
-# dp 저장 변수([0]: 가장 긴 수열 길이 저장, [1]: 증가 수열 요소 저장)
-d = [[0, []] for i in range(N)]
+d = [0] * N     # LIS 길이
+v = [-1] * N    # 역추적 용도
 
 for i in range(N):
-    d[i][0] = 1             # 자기 자신 +1
-    d[i][1].append(A[i])    # 수열 요소 추가
+    d[i] = 1    # 자기 자신 +1
     # 처음부터 i 요소까지 반복
     for j in range(i):
-        # i 요소가 j 요소보다 크고, 현재 수열 길이보다 길면
-        if A[j] < A[i] and d[i][0] < d[j][0] + 1:
-            d[i][0] = d[j][0] + 1       # 수열 길이 업데이트
-            d[i][1] = d[j][1] + [A[i]]  # 이전 증수 수열 + 현재 수열 요소
+        # 현재 수열의 요소가 크고, LIS 길이도 길면
+        if A[j] < A[i] and d[i] < d[j] + 1:
+            d[i] = d[j] + 1 # LIS 업데이트
+            v[i] = j        # 역추적을 위해 이전 인덱스 저장
 
-answer = max(d, key=lambda x:x[0])
+answer = max(d)
 
-print(answer[0])
-print(*answer[1])
+print(answer)
+go(d.index(answer))
