760ab11746a1641aa83625070472b7de527f8833
subslog <100672617+subslog@users.noreply.github.com>
2022-06-29 02:28:02 +0900

    [Silver II] Title: 제곱수의 합, Time: 4324 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -3,11 +3,14 @@ N = int(input())
 d = [0] * (N + 1) # DP 저장 리스트
 
 # 점화식 : D[N] = min(D[N - i**2]) + 1
-for i in range(1, N + 1):
-    d[i] = i    # N의 제곱수 최대항은 N이다.
-    # 루트 N까지 반복
-    for j in range(1, int(i ** 0.5) + 1):
-        # D[N - i**2] + 1 보다 크면 업데이트
-        if d[i] > d[i - j*j] + 1: d[i] = d[i - j*j] + 1
+if int(N ** 0.5) ** 2 == N:
+    print(1)
+else:
+    for i in range(1, N + 1):
+        d[i] = i    # N의 제곱수 최대항은 N이다.
+        # 루트 N까지 반복
+        for j in range(1, int(i ** 0.5) + 1):
+            # D[N - i**2] + 1 보다 크면 업데이트
+            if d[i] > d[i - j*j] + 1: d[i] = d[i - j*j] + 1
 
-print(d[N])
+    print(d[N])
