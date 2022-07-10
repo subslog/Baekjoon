615ffb4a49cda889d137c2b071f2bf4c86843427
subslog <100672617+subslog@users.noreply.github.com>
2022-07-10 21:38:00 +0900

    [Silver III] Title: 사탕 게임, Time: 144 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,11 +1,12 @@
 import sys
 
-def len_check(temp_board: list) -> int:
+def len_check(temp_board: list, start_line: int, end_line: int, start_row: int, end_row: int) -> int:
     """최대 길이 구하는 함수"""
     max_len = 1 # 최대 길이
 
-    for i in range(N):
-        cnt = 1     # 행 최대 길이 카운트
+    # 행 최대 길이 카운트
+    for i in range(start_line, end_line + 1):
+        cnt = 1
 
         # i행에서 최대 길이 구하기
         for j in range(N - 1):
@@ -17,8 +18,10 @@ def len_check(temp_board: list) -> int:
                 cnt = 1
             # 카운트가 최대 길이보다 크면 업데이트
             if cnt > max_len: max_len = cnt
-        
-        cnt = 1     # 열 최대 길이 카운트
+    
+    # 열 최대 길이 카운트
+    for i in range(start_row, end_row + 1):
+        cnt = 1
 
         # i열에서 최대 길이 구하기
         for j in range(N - 1):
@@ -34,12 +37,8 @@ def len_check(temp_board: list) -> int:
     return max_len
 
 N = int(input())
-board = []
-answer = 1
-
-for _ in range(N):
-    candy = list(sys.stdin.readline().rstrip())
-    board.append(candy)
+board = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
+answer = len_check(board, 0, N - 1, 0, N - 1)
 
 # 모든 요소를 비교
 for i in range(N):
@@ -49,7 +48,7 @@ for i in range(N):
             # 현재 요소와 아래 요소 변경
             board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
             # 변경 후 최대 길이 확인
-            tmp = len_check(board)
+            tmp = len_check(board, i, i + 1, j, j)
             if answer < tmp: answer = tmp
             # 보드 원상 복구
             board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
@@ -58,7 +57,7 @@ for i in range(N):
             # 현재 요소와 오른쪽 요소 변경
             board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
             # 변경 후 최대 길이 확인
-            tmp = len_check(board)
+            tmp = len_check(board, i, i, j, j + 1)
             if answer < tmp: answer = tmp
             # 보드 원상 복구
             board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
