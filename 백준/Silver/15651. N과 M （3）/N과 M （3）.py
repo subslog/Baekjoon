f84bb3f352e29cc078505e5bb26c9d72b4d8cf08
subslog <100672617+subslog@users.noreply.github.com>
2022-07-14 00:32:02 +0900

    [Silver III] Title: N과 M (3), Time: 236 ms, Memory: 99816 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,7 @@
-def n_m(index: list, N: int, M: int):
-    # M개의 수열을 뽑았으면 출력 후 종료
-    if index == M:
-        print(*a[:index])
-        return
-    # 중복 있게 재귀
-    for i in range(1, N + 1):
-        a[index] = i
-        n_m(index + 1, N, M)
+from itertools import product
 
 N, M = map(int, input().split())
 
-# M개 수열 뽑기 체크
-a = [0] * 8
-n_m(0, N, M)
+num = map(str, range(1, N + 1))
+
+print("\n".join(list(map(" ".join, product(num, repeat=M)))))
