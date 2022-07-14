81df303151746a462fdfc8080b1a7a2014aaca62
subslog <100672617+subslog@users.noreply.github.com>
2022-07-14 23:40:20 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 54652 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,19 +1,7 @@
-from collections import Counter
-
-def n_m(index, N, M):
-    # M개의 수열을 뽑으면 출력 후 재귀 종료
-    if index == M:
-        print(*a[:index])
-        return
-    
-    for i in num:
-        a[index] = i         # 수열 넣기
-        n_m(index + 1, N, M) # 다음 수열 작업
+from itertools import product
 
 N, M = map(int, input().split())
 
 # 중복 제거 후 정렬
-num = sorted(map(int, set(input().split())))
-
-a = [0] * 9
-n_m(0, N, M)
+num = map(str, sorted(map(int, set(input().split()))))
+print("\n".join(map(" ".join, product(num, repeat=M))))
