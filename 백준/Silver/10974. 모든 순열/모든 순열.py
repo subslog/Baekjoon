e763475d7c22e170750a4ce5fab6a3e95ee817db
subslog <100672617+subslog@users.noreply.github.com>
2022-07-15 01:41:25 +0900

    [Silver III] Title: 모든 순열, Time: 212 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,19 +1,26 @@
-def permutation(index: int, N: int):
-    # N개의 순열을 뽑았으면 출력 후 재귀 종료
-    if index == N:
-        print(*a[:index])
-        return
+def next_permutation(arr: list, N: int):
+    """다음 수열을 구하는 함수"""
+    i = N - 1
+    # A[i-1] < A[i]를 만족하는 가장 큰 i를 찾는다.
+    while i > 0 and arr[i - 1] >= arr[i]: i -= 1
+    # 내림차순 정렬된 수열로 False 반환
+    if i <= 0: return False
+    # i <= j 이면서 A[i - 1] < A[j]를 만족하는 가장큰 j 찾기
+    last = i - 1
+    for j in range(last + 1, N):
+        if arr[last] < arr[j]:
+            exchange = j
+    # A[i-1]와 A[j]를 swap
+    arr[last], arr[exchange] = arr[exchange], arr[last]
+    # A[i]부터 순열을 뒤집는다.
+    arr[last + 1:] = arr[-1:last:-1]
 
-    for i in range(1, N + 1):
-        if c[i]: continue           # 중복 순열 반복 제외
-        c[i] = True                 # 순열 사용
-        a[index] = i                # 순열 저장
-        permutation(index + 1, N)   # 다음 순열 저장
-        c[i] = False                # 순열 사용 끝
+    return True
 
 N = int(input())
+arr = list(range(1, N + 1))
 
-c = [False] * 9 # 중복 순열 체크
-a = [0] * 9     # 순열 저장
-
-permutation(0, N)
+while True:
+    print(*arr)
+    if not next_permutation(arr, N):
+        break
