afbd8c9ac47eea800748909eb849e44a001c70f6
subslog <100672617+subslog@users.noreply.github.com>
2022-08-04 00:39:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 808 ms, Memory: 64500 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,15 +1,14 @@
 import sys
-sys.setrecursionlimit(1000000)
 
-def dfs(visit: int):
-    """dfs 함수"""
-    # 방문 노드 체크
-    check[visit] = True
-    # 인접 노드 재귀 호출로 방문
-    for i in a[visit]:
-        # 방문하지 않은 노드라면 방문
-        if check[i] == False:
-            dfs(i)
+#def dfs(visit: int):
+#    """dfs 함수"""
+#    # 방문 노드 체크
+#    check[visit] = True
+#    # 인접 노드 재귀 호출로 방문
+#    for i in a[visit]:
+#        # 방문하지 않은 노드라면 방문
+#        if check[i] == False:
+#            dfs(i)
 
 N, M = map(int, input().split())
 
@@ -19,6 +18,8 @@ a = [[] for _ in range(N + 1)]
 check = [False] * (N + 1)
 # 연결 요소 카운트
 cnt = 0
+# dfs 저장용 스택
+s = []
 
 # 그래프 생성
 for _ in range(M):
@@ -30,7 +31,18 @@ for _ in range(M):
 for i in range(1, N + 1):
     # 방문하지 않은 노드면 dfs 수행 후 연결 요소 +1
     if check[i] == False:
-        dfs(i)
+        # 방문 노드 체크 후 스택에 추가
+        check[i] = True
+        s.append(i)
+        # 스택이 없어질 때까지 반복
+        while len(s) != 0:
+            # 현재 노드
+            x = s.pop()
+            # 다음 방문 노드 스택에 추가
+            for j in a[x]:
+                if check[j] == False:
+                    check[j] = True
+                    s.append(j)
         cnt += 1
 
 print(cnt)
