cf83dfa74460229c0c2ebc5d49f6c6e20f07dd04
subslog <100672617+subslog@users.noreply.github.com>
2022-08-11 01:41:31 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 772 ms, Memory: 31860 KB -BaekjoonHub

---
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.py
@@ -0,0 +1,47 @@
+class Node:
+    """이진 트리 저장 클래스"""
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+def inorder(x, level):
+    """중위 순회"""
+    # 자식 노드가 없으면 순회 종료
+    if x == -1: return
+    # 레벨 키가 사전에 없으면 추가
+    if not level in width: width[level] = []
+
+    inorder(a[x].left, level + 1)
+    # 노드의 열 번호 증가 후 삽입
+    global cnt
+    cnt += 1
+    width[level].append(cnt)
+    inorder(a[x].right, level + 1)
+    
+
+N = int(input())
+cnt = [0] * (N + 1) # 루트 찾기용
+# 그래프 생성
+a = [0] * (N + 1)
+for _ in range(1, N + 1):
+    x, y, z = map(int, input().split())
+    a[x] = Node(y, z)
+    # 부모가 있으면 카운트
+    if y != -1: cnt[y] += 1
+    if z != -1: cnt[z] += 1
+# 원소값이 0이면 루트
+root = 1
+for i in range(1, N + 1):
+    if cnt[i] == 0: root = i
+
+# 각 레벨의 노드 인덱스 저장용
+width = {}
+# 너비 카운트
+cnt = 0
+
+inorder(root, 1)
+# 각 레벨의 최대 너비 계산
+for i in width:
+    width[i] = max(width[i]) - min(width[i]) + 1
+# 내림차순 정렬
+answer = sorted(width, key=lambda x: width[x], reverse=True)
+print(answer[0], width[answer[0]])
