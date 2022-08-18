07643537ec54976dd6acb7622d8e1755e3e55914
subslog <100672617+subslog@users.noreply.github.com>
2022-08-19 02:04:18 +0900

    [Silver II] Title: 부분수열의 합, Time: 460 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,13 +1,21 @@
+def subsequence(index: int, sum):
+    """부분 수열의 합이 S와 같은 경우의 수를 구하는 함수"""
+    global ans
+    # 재귀 종료 조건 : 수열의 마지막 인덱스까지 재귀 완료
+    if index == N:
+        # 정답 조건 : 부분 수열의 합이 S와 같으면 카운트
+        if sum == S:
+            ans += 1
+        return
+    # 선택하는 경우
+    subsequence(index + 1, sum + arr[index])
+    # 선택하지 않는 경우
+    subsequence(index + 1, sum)
+
 N, S = map(int, input().split())
-a = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+ans = 0
+subsequence(0, 0)
+if S == 0: ans -= 1
 
-answer = 0
-
-# 함수 반복
-for i in range(1, (1 << N)):
-    s = 0
-    # 00000 ~ 11111 까지 부분 수열을 더해 S와 같으면 answer +1
-    s = sum(a[j] for j in range(N) if i & (1 << j) > 0)
-    if S == s: answer += 1
-
-print(answer)
+print(ans)
