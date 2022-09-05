7d986cb259512b0256d4442c096e4790641619e5
subslog <100672617+subslog@users.noreply.github.com>
2022-09-06 01:02:26 +0900

    [Gold IV] Title: 소수 경로, Time: 136 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Gold/1963. 소수 경로/소수 경로.py
+++ 백준/Gold/1963. 소수 경로/소수 경로.py
@@ -9,15 +9,22 @@ for i in range(2, int(10000 ** 0.5) + 1):
     if prime_check[i]:
         for j in range(i + i, 10000, i):
             prime_check[j] = False
+# 각 소수에서 한 자리 차이나는 소수 추가
+arr = [[] for _ in range(10000)]
+for i in range(1000, 10000):
+    if prime_check[i]:
+        for idx in range(4):
+            for n in range(10):
+                if idx == 0 and n == 0: continue
+                s = list(str(i))
+                s[idx] = str(n)
+                num = int(''.join(s))
+                if i != num and prime_check[num]:
+                    arr[i].append(num)
 
 T = int(input())
 for _ in range(T):
     start, end = map(int, sys.stdin.readline().split())
-    # start ~ end 사이의 소수 리스트 생성
-    prime = []
-    for i in range(1000, 10000):
-        if prime_check[i]:
-            prime.append(i)
     # 방문 처리용
     dist = [-1] * 10000
     # 시작 위치 처리
@@ -29,17 +36,9 @@ for _ in range(T):
         b = queue.popleft()
         # 목표 소수가 나오면 반복 종료
         if end == b: break
-        b1000, b100, b10, b1 = str(b)
-        # 소수 자리수 비교
-        for p in prime:
-            cnt = 0
-            p1000, p100, p10, p1 = str(p)
-            if b1000 == p1000: cnt += 1
-            if b100 == p100: cnt += 1
-            if b10 == p10: cnt += 1
-            if b1 == p1: cnt += 1
-            # 세 자리가 동일하고 방문하지 않았으면 방문
-            if cnt == 3 and dist[p] == -1:
+        # 방문하지 않은 숫자는 방문
+        for p in arr[b]:
+            if dist[p] == -1:
                 dist[p] = dist[b] + 1
                 queue.append(p)
 
