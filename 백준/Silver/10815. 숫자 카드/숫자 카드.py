53bfe747d91c1b0e6f923230ec935fa2f1b37249
subslog <100672617+subslog@users.noreply.github.com>
2022-09-13 22:18:03 +0900

    [Silver V] Title: 숫자 카드, Time: 652 ms, Memory: 230000 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,4 +1,21 @@
-import bisect
+def binary_search(arr: list, num: int):
+    """정렬된 리스트에서 num이 존재하는지 탐색하는 이진 탐색 함수"""
+    l, r = 0, len(arr) - 1
+    # 왼쪽 인덱스가 오른쪽 인덱스보다 커질 때까지 반복
+    while l <= r:
+        mid = (l + r) // 2  # 중간값
+        # 찾으면 1 반환
+        if arr[mid] == num:
+            return 1
+        # 찾는 수가 더 작으면 r 업데이트
+        elif arr[mid] > num:
+            r = mid - 1
+        # 찾는 수가 더 크면 l 업데이트
+        else:
+            l = mid + 1
+    # 없는 수면 0 반환
+    return 0
+
 # 초기값 입력
 N = int(input())
 card_list = list(map(int, input().split()))
@@ -11,11 +28,6 @@ card_list.sort()
 # 이진 탐색 수행
 answer = []
 for c in check_list:
-    result = bisect.bisect_right(card_list, c) - bisect.bisect_left(card_list, c)
-    # 찾는 카드가 있으면 1, 없으면 0
-    if result == 1:
-        answer.append(1)
-    else:
-        answer.append(0)
+    answer.append(binary_search(card_list, c))
 
 print(*answer)
