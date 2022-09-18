71255188deef350881f2ecd3bd26cab3f3065c4f
subslog <100672617+subslog@users.noreply.github.com>
2022-09-19 04:00:03 +0900

    [Silver I] Title: 지름길, Time: 92 ms, Memory: 32416 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+INF = int(1e9)
+
+# 초기값 입력
+N, D = map(int, input().split())
+shortcut = [tuple(map(int, input().split())) for _ in range(N)]
+# 지름길 시작 위치 순으로 오름차순 정렬
+shortcut.sort()
+# 고속도로 길이
+road = [i for i in range(D + 1)]
+
+# DP 수행
+for i in range(1, D + 1):
+    road[i] = min(road[i], road[i - 1] + 1)
+    # 지름길이 빠르면 업데이트
+    for s, e, d in shortcut:
+        if s >= 0 and e == i and road[s] + d < road[e]:
+            road[e] = road[s] + d
+
+print(road[D])
