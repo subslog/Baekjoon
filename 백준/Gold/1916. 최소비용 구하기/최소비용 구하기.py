1a54634fad565a9d946224dc471482e266abffc8
subslog <100672617+subslog@users.noreply.github.com>
2022-09-19 22:25:06 +0900

    [Gold V] Title: 최소비용 구하기, Time: 368 ms, Memory: 52560 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+INF = int(1e9)
+
+def dijkstra(arr: list, start: int, dist: list):
+    """start에서 모든 경로로 가는 최소 비용을 구하는 함수"""
+    # 시작 노드 처리
+    queue = []
+    heapq.heappush(queue, (0, start))
+    dist[start] = 0
+
+    # 최단 경로 계산
+    while queue:
+        # 현재 비용, 노드
+        d, now = heapq.heappop(queue)
+        # 이미 방문된 노드라면 건너뛴다.
+        if dist[now] < d:
+            continue
+        for a in arr[now]:
+            # 현재 노드를 거쳐 가는 비용이 작으면 업데이트
+            if dist[now] + a[0] < dist[a[1]]:
+                dist[a[1]] = dist[now] + a[0]
+                heapq.heappush(queue, (a[0], a[1]))
+
+# 초기값 입력 및 그래프 생성
+N = int(input())
+M = int(input())
+citys = [[] for _ in range(N + 1)]
+for _ in range(M):
+    s, e, d = map(int, sys.stdin.readline().split())
+    citys[s].append((d, e))
+start, end = map(int, input().split())
+# 모든 비용을 무한으로 설정
+dist = [INF] * (N + 1)
+
+# start -> end 최소 비용
+dijkstra(citys, start, dist)
+print(dist[end])
