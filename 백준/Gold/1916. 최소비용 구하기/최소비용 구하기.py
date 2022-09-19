9f99dc6e192df4ae9064c14337ac0daed99a3cb4
subslog <100672617+subslog@users.noreply.github.com>
2022-09-19 22:30:32 +0900

    [Gold V] Title: 최소비용 구하기, Time: 340 ms, Memory: 55632 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -11,16 +11,18 @@ def dijkstra(arr: list, start: int, dist: list):
 
     # 최단 경로 계산
     while queue:
-        # 현재 비용, 노드
+        # 현재 비용이 제일 작은 노드
         d, now = heapq.heappop(queue)
         # 이미 방문된 노드라면 건너뛴다.
         if dist[now] < d:
             continue
+        # 현재 노드와 인접한 노드 확인
         for a in arr[now]:
             # 현재 노드를 거쳐 가는 비용이 작으면 업데이트
-            if dist[now] + a[0] < dist[a[1]]:
-                dist[a[1]] = dist[now] + a[0]
-                heapq.heappush(queue, (a[0], a[1]))
+            cost = d + a[0]
+            if cost < dist[a[1]]:
+                dist[a[1]] = cost
+                heapq.heappush(queue, (cost, a[1]))
 
 # 초기값 입력 및 그래프 생성
 N = int(input())
