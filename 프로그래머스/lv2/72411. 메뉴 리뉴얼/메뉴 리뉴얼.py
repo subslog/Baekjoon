4f8bb60d59ff2bc315dc7b8e79abf4a8e6c64bbe
subslog <100672617+subslog@users.noreply.github.com>
2022-09-21 03:50:36 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -9,14 +9,14 @@ def solution(orders, course):
         # 갯수를 기준으로 조합
         for order in orders:
             # 정렬 후 주문 메뉴를 cnt 개로 조합
-            group += list(map(lambda x: "".join(x), combinations(sorted(list(order)), cnt)))
+            group += list(map(lambda x: "".join(x), combinations(sorted(order), cnt)))
         # 만족하는 조합이 없으면 건너뛰기
         if not group: continue
-        # 조합 메뉴를 키로 카운트
+        # 조합 메뉴를 키로 카운트 후 주문순으로 내림차순 정렬
         group = Counter(group).most_common()
-        # 제일 많이 시킨 조합
-        max_cnt = max(group, key=lambda x: x[1])[1]
         # 2번 이상 시킨 조합이면서 최대값인 조합 모두 추가
-        answer += [i[0] for i in filter(lambda x: x[1] >= 2 and x[1] == max_cnt, group)]
+        answer += [''.join(i) for i, j in group if j >= 2 and j == group[0][1]]
         
     return sorted(answer)
+
+print(solution(["XYZ", "XWY", "WXA"], [2, 3, 4]))
