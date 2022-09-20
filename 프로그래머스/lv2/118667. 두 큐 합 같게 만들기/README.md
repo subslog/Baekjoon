827636b014bdca6c7a3b970537330af710effcc0
subslog <100672617+subslog@users.noreply.github.com>
2022-09-21 04:52:59 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 63.36 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// queue1_len은 배열 queue1의 길이입니다.
+// queue2_len은 배열 queue2의 길이입니다.
+int solution(int queue1[], size_t queue1_len, int queue2[], size_t queue2_len) {
+    int answer = -2;
+    return answer;
+}
