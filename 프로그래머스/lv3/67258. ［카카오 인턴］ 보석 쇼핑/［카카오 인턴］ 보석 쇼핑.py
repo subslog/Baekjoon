17766589d5ebb753841625060325512203015fff
subslog <100672617+subslog@users.noreply.github.com>
2022-09-21 21:55:35 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 57.33 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,67 +1,38 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
 def solution(gems):
     answer = [1, len(gems)]
-    # 보석을 키로 딕셔너리 생성
+    # 수집된 보석 저장
     gems_dict = dict()
-    for gem in gems:
-        if not gem in gems_dict:
-            gems_dict[gem] = 0
-    # 보석 개수
-    gem_cnt = len(gems_dict)
-    # 모든 보석을 확인
-    start, end = 0, 0
-    cnt = 0             # 수집된 보석 수
-    result = len(gems)  # 구간 길이
-    for gem in gems:
-        # 처음으로 포함되는 보석이면 수집된 보석 수 증가
-        if gems_dict[gem] == 0:
-            cnt += 1
-        # 보석을 추가하고 end를 증가한다.
-        gems_dict[gem] += 1
-        end += 1
-        # start 위치의 보석이 2개 이상 포함되어 있으면 시작 위치를 증가한다.
-        for i in range(start, end):
+    # 보석의 수
+    gem_cnt = len(set(gems))
+    # gems 길이
+    lenth = len(gems)
+    # 보석 구매
+    start, end = 0, 0   # 보석 수집 구간
+    result = lenth      # 최소 구간 저장
+    while start < lenth and end <= lenth:
+        # 모든 보석이 수집되지 않았으면 end 증가
+        if len(gems_dict) < gem_cnt:
+            if end >= lenth:
+                break
+            # 수집된 보석이면 수 증가
+            if gems[end] in gems_dict:
+                gems_dict[gems[end]] += 1
+            # 처음 수집되는 보석이면 추가
+            else:
+                gems_dict[gems[end]] = 1
+            end += 1
+        # 모든 보석이 수집되면 start 증가
+        else:
+            # 현재 구간이 짧으면 구간 갱신
+            if end - start < result:
+                result = end - start
+                answer = [start + 1, end]
+            # start 위치의 보석의 수가 2개 이상이면 감소
             if gems_dict[gems[start]] > 1:
                 gems_dict[gems[start]] -= 1
-                start += 1
+            # 1개만 있으면 보석 삭제
             else:
-                break
-        # 모든 보석이 수집되고, 최소 구간이면 구간 갱신
-        if cnt == gem_cnt and end - start < result:
-            result = end - start
-            answer = [start + 1, end]
-
+                del gems_dict[gems[start]]
+            start += 1
+            
     return answer
