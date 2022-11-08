66c98034186b60aa64834ec996a571453a538d72
subslog <100672617+subslog@users.noreply.github.com>
2022-11-09 00:16:29 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,32 +1,22 @@
+import re
+
 def solution(dartResult):
-    bonus = {'S':1, 'D':2, 'T':3}   # 각 점수를 계산할 보너스
-    scores = []                     # 각 라운드 점수 저장
-    score = ''
-    
-    round = -1
-    # 다트 결과 반복
-    for i in dartResult:
-        # 숫자면 점수 추가
-        if i.isdecimal():
-            score += i
-        # 알파벳이면
-        elif i.isalpha():
-            # 점수 추가 후 초기화
-            scores.append(int(score))
-            score = ''
-            # 현재 라운드
-            round += 1
-            # 보너스 계산
-            scores[round] **= bonus[i]
-        # 스타상(*)
-        elif i == '*':
-            # 첫 번째 라운드가 아니면 이전 라운드 점수도 *2
-            if round != 0: scores[round - 1] *= 2
-            scores[round] *= 2
-        # 아차상(#)
-        elif i == '#':
-            scores[round] *= -1
-    
-    answer = sum(scores)
-    
-    return answer
+    # 보너스 제곱
+    bonus = {'S':1, 'D':2, 'T':3}
+    # 다트 결과를 3라운드로 분리
+    round = re.findall('(1*[0-9])([SDT])([*#]*)', dartResult)
+    # 각 라운드 점수
+    score = [0, 0, 0, 0]
+
+    # 다트 점수 계산
+    for i in range(3):
+        # 점수에 보너스 점수만큼 제곱
+        score[i + 1] = int(round[i][0]) ** bonus[round[i][1]]
+        # 옵션이 있으면 적용
+        if round[i][2] == '*':
+            score[i] *= 2
+            score[i + 1] *= 2
+        elif round[i][2] == '#':
+            score[i + 1] *= -1
+
+    return sum(score[1:])
