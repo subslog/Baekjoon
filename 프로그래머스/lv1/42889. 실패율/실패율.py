c59925ea3dc6f5caf45d03ca6de0682775672632
subslog <100672617+subslog@users.noreply.github.com>
2022-11-08 23:34:02 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,27 +1,19 @@
 def solution(N, stages):
-    # 정답 저장 리스트
-    answer = []
-    # 각 스테이지에 머물러 있는 플레이어 리스트
-    stay = [0] * (N + 2)
-    # 실패율 저장 리스트
-    fail = []
-    
-    # fail 리스트 카운트
-    for i in stages:
-        stay[i] += 1
-    
+    # 각 스테이지를 도달 및 도전 중인 사용자 수
+    arrival_cnt = [0] * (N + 2)
+    stages_cnt = [0] * (N + 2)
+    for stage in stages:
+        stages_cnt[stage] += 1
+        # 현재 도전 중인 스테이지 이하는 모두 도달
+        for round in range(1, stage + 1):
+            arrival_cnt[round] += 1
     # 실패율 계산
+    stages_failure = {i:0 for i in range(1, N + 1)}
     for i in range(1, N + 1):
-        # 현재 스테이지를 도달한 플레이어 수
-        success = sum(stay[i:])
-        # 0이면 도달한 플레이어가 없다.
-        if success == 0:
-            fail.append((i, 0))
-        else:
-            fail.append((i, stay[i] / success))
-    
-    # 실패율 기준으로 정렬
-    for i in sorted(fail, reverse=True, key=lambda x: x[1]):
-        answer.append(i[0])
+        # 도달한 사용자가 있는 스테이지만 계산
+        if 0 < arrival_cnt[i]:
+            stages_failure[i] = stages_cnt[i] / arrival_cnt[i]
+    # 실패율 오름차순 정렬해 스테이지 저장
+    answer = sorted(stages_failure, key=lambda x: stages_failure[x], reverse=True)
 
     return answer
