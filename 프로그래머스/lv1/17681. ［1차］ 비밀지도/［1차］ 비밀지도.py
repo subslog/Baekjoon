920dd9e6a9a33c9f9f7be312a99ed94d4a6987d9
subslog <100672617+subslog@users.noreply.github.com>
2022-11-09 00:25:48 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -3,7 +3,8 @@ def solution(n, arr1, arr2):
     # 지도1과 지도2를 or 연산하여 결과에 따라 저장
     for i in range(n):
         # or 연산 후 0으로 자리수 채우기
-        result = bin(arr1[i] | arr2[i]).lstrip('0b').rjust(n, '0').replace('1', '#').replace('0', ' ')
-        answer.append(result)
+        result = bin(arr1[i] | arr2[i]).lstrip('0b').rjust(n, '0')
+        # 1 -> #, 0 -> ' ' 변환
+        answer.append(result.replace('1', '#').replace('0', ' '))
         
     return answer
