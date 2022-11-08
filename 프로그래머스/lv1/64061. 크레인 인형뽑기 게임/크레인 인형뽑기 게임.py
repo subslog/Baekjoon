8b342b196cf092bf047e9e67550710f3b4ecb32a
subslog <100672617+subslog@users.noreply.github.com>
2022-11-08 22:50:52 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.23 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,23 +1,21 @@
 def solution(board, moves):
     answer = 0
+    # 뽑은 인형을 담을 리스트
     basket = []
-
-    # 인형 뽑기
+    # 인형을 뽑는다.
     for move in moves:
-        # move - 1 열에서 반복
-        for i in range(len(board)):
-            # 인형이 있으면
-            if board[i][move - 1] != 0:
-                # 바구니에 담기
-                basket.append(board[i][move - 1])
-                # 인형 제거
-                board[i][move - 1] = 0
-                # 바구니에 똑같은 인형이 있으면 점수 +2
-                if len(basket) >= 2 and basket[-1] == basket[-2]:
+        # 인형이 나올 때까지 크레인을 내린다.
+        for depth in range(len(board)):
+            # 인형이 나오면 뽑는다.
+            if 0 < board[depth][move - 1]:
+                basket.append(board[depth][move - 1])
+                board[depth][move - 1] = 0
+                # 인형이 2개 이상이고, 같은 인형이 연속되면 터트린다.
+                if 2 <= len(basket) and basket[-1] == basket[-2]:
                     basket.pop()
                     basket.pop()
                     answer += 2
-                # 다음 뽑기 진행
+                # 다음 크레인 위치로 이동
                 break
-    
+        
     return answer
