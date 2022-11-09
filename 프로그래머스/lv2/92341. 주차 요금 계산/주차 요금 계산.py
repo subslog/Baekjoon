66fae9e7ed9f3a568dbfa48816cc167c444083e0
subslog <100672617+subslog@users.noreply.github.com>
2022-11-10 01:38:28 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,51 +1,46 @@
-def time_change(time: str):
-    """시간을 분으로 환산하여 반환하는 함수"""
-    hour, minute = map(int, time.split(':'))
-    
-    return hour * 60 + minute
+import math
 
-def parking_cal(d_time: int, d_won: int, time: int, won: int, p_time : int):
-    """주차 요금을 계산하는 함수"""
-    money = d_won       # 기본 요금
-    p_time -= d_time    # 기본 시간 빼기
-    # 기본 시간을 초과하면 추가 계산
-    if p_time > 0:
-        money += p_time // time * won
-        # 잔여 시간이 있으면 추가 계산
-        if p_time % time > 0:
-            money += won
-    
-    return money
+def parking_time(start: str, end: str) -> int:
+    """주차 시간을 분으로 반환하는 함수"""
+    # 주차 (분)
+    s_h, s_m = start.split(':')
+    start_m = int(s_h) * 60 + int(s_m)
+    # 출차 (분)
+    e_h, e_m = end.split(':')
+    end_m = int(e_h) * 60 + int(e_m)
+
+    return end_m - start_m
 
 def solution(fees, records):
     answer = []
-    parking = [-1] * 10000      # 차량 입차, 출차 체크용 리스트
-    total = [0] * 10000         # 토탈 시간 저장용 리스트
-    
-    # 입차, 출차 처리
+
+    parking = dict()    # 주차 차량
+    settlement = dict() # 주차 시간
+
+    # 주차 요금 정산
     for record in records:
-        # 시간, 차량 번호, (입차 or 출차)
-        time, number, state = record.split()
-        number = int(number)
-        # 시간을 분으로 환산
-        minute = time_change(time)
-        # 입차면 parking 리스트에 시간 추가
-        if state == 'IN':
-            parking[number] = minute
-        # 출차면 parking 리스트에서 제거 후 total 리스트에 누적
+        # 시각, 차량번호, 내역
+        time, number, history = record.split()
+        # 주차 처리
+        if history == 'IN':
+            parking[number] = time
+            # 주차 정산 시간에 차가 없으면 추가
+            if number not in settlement: settlement[number] = 0
+        # 출차 처리
         else:
-            total[number] += minute - parking[number]
-            parking[number] = -1
-    
-    # 요금 계산
-    minute = time_change('23:59')
-    for i in range(10000):
-        # 출차를 안했으면 출차 시간 23:59 으로 계산
-        if parking[i] != -1:
-            total[i] += minute - parking[i]
-            parking[i] = -1
-        # 주차 요금 계산
-        if total[i] > 0:
-            answer.append(parking_cal(fees[0], fees[1], fees[2], fees[3], total[i]))
-    
+            # 주차 시간
+            settlement[number] += parking_time(parking.pop(number), time)
+    # 출차하지 않은 차 출차 처리
+    for exit in parking:
+        settlement[exit] += parking_time(parking[exit], '23:59')
+    # 차량 번호 오름차순으로 주차비 정산
+    for total in sorted(settlement):
+        # 기본 요금
+        fee = fees[1]
+        # 잔여 시간 = 주차 시간 - 기본 시간
+        fee_time = settlement[total] - fees[0]
+        # 단위 요금 정산 (잔여 시간이 마이너스면 추가 요금이 없다.)
+        fee += max(0, math.ceil(fee_time / fees[2]) * fees[3])
+        answer.append(fee)
+
     return answer
